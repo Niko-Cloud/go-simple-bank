262abc72052c05de166901a0df4bfbb70d77ed15
Niko-Cloud <indraniko20@gmail.com>
2025-05-14 23:05:37 +0700

    Using viper to use environment variable

--- db/sqlc/test/main_test.go
+++ db/sqlc/test/main_test.go
@@ -7,21 +7,20 @@ import (
 	"os"
 	_ "simplebank/app/db/sqlc/gen"
 	db "simplebank/app/db/sqlc/gen"
+	"simplebank/app/db/util"
 	"testing"
 )
 
 var testQueries *db.Queries
 var testDB *sql.DB
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
-	var err error
+	config, err := util.LoadConfig("../../..")
+	if err != nil {
+		log.Fatal("Cannot load config:", err)
+	}
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
--- main.go
+++ main.go
@@ -6,24 +6,25 @@ import (
 	"log"
 	"simplebank/app/api"
 	db "simplebank/app/db/sqlc/gen"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"simplebank/app/db/util"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
 	if err != nil {
+		log.Fatal("Cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+
 		log.Fatal("cannot connect to db:", err)
 	}
 
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}

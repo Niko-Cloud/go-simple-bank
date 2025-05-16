0b42b78f43ad1acf1136adf9f2884214b6f63051
Niko-Cloud <indraniko20@gmail.com>
2025-05-16 23:03:24 +0700

    Add JWT Token

---
+++ token/payload.go
@@ -0,0 +1,62 @@
+package token
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"time"
+)
+
+var (
+	ErrInvalidToken = errors.New("token is invalid")
+	ErrExpiredToken = errors.New("token has expired")
+)
+
+// Payload contains the payload data of the token
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiresAt time.Time `json:"expires_at"`
+}
+
+func (payload *Payload) GetExpirationTime() (*jwt.NumericDate, error) {
+	return jwt.NewNumericDate(payload.ExpiresAt), nil
+}
+
+func (payload *Payload) GetIssuedAt() (*jwt.NumericDate, error) {
+	return jwt.NewNumericDate(payload.IssuedAt), nil
+}
+
+func (payload *Payload) GetNotBefore() (*jwt.NumericDate, error) {
+	return jwt.NewNumericDate(payload.IssuedAt), nil
+}
+
+func (payload *Payload) GetIssuer() (string, error) {
+	return "", nil
+}
+
+func (payload *Payload) GetSubject() (string, error) {
+	return payload.Username, nil
+}
+
+func (payload *Payload) GetAudience() (jwt.ClaimStrings, error) {
+	return nil, nil
+}
+
+// NewPayload creates a new token payload with a specific username and duration
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiresAt: time.Now().Add(duration),
+	}
+
+	return payload, nil
+}

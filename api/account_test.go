c288caeced3417f8e5242ba76ab93db2b298d8c9
Niko-Cloud <indraniko20@gmail.com>
2025-05-17 19:18:09 +0700

    Fix deprecated

--- api/account_test.go
+++ api/account_test.go
@@ -13,7 +13,6 @@ import (
 	"github.com/yuki/simplebank/token"
 	"github.com/yuki/simplebank/util"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -413,7 +412,7 @@ func requireBodyMatchAccount(t *testing.T, body *bytes.Buffer, account db.Accoun
 }
 
 func requireBodyMatchAccounts(t *testing.T, body *bytes.Buffer, accounts []db.Account) {
-	data, err := ioutil.ReadAll(body)
+	data, err := io.ReadAll(body)
 	require.NoError(t, err)
 
 	var gotAccounts []db.Account
